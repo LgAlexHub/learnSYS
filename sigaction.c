268135db9516ceaf8f4b430a6c74faa97f4d9f15
Legras Alexis <21801364@C304L-401C17.campus.unicaen.fr>
2020-09-09 13:20:49 +0200

    fini sigaction

--- sigaction.c
+++ sigaction.c
@@ -2,21 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
+
 //SIGFPE
 
-void error_fatal(int sig);
-void error_1(int sig){
-	printf("Pas de division par 0 derniere avertissement \n");
-	signal(sig,error_fatal);
+struct sigaction act;
+struct sigaction act2;
+
+static void hdl1(int sig, siginfo_t *siginfo, void *context){
+	printf("Interdit de faire des divisions par 0\n");
+	sigaction(SIGFPE,&act2,NULL);
 }
 
-void error_fatal(int sig){
+
+
+static void error_fatal(int sig, siginfo_t *siginfo, void *context){
 	printf("Vous n'avez rien compris je me tire \n");
 	exit(0);
 }
 
 int main(void){
-	signal(SIGFPE,error_1);
+	memset(&act, '\0', sizeof(act));
+	memset(&act, '\0', sizeof(act2));
+	act2.sa_sigaction = &error_fatal;
+	act.sa_sigaction = &hdl1;
+	sigaction(SIGFPE,&act,NULL);
 	int c = 3/0;
 	sleep(5);
 	int d = 4/0;

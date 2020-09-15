df171c55d0b9b4183deef41dcde42f8da33dca81
Alexis Legras <alexlegras@hotmail.com>
2020-09-15 21:51:04 +0200

    flemme

---
+++ tube_bi_pere.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void){
+    int pip[2];
+    int pip2[2];
+    pid_t pid;
+    char chaine[]="Avant...";
+    int taille;
+    pipe(pip);
+
+    if((pid=fork())==0){
+        sleep(2);
+        char chaine_fils[]="Pour toi papa :)";
+        close(pip[0]);
+        close(pip2[1]);
+        write(pip[1],&chaine_fils,strlen(chaine_fils));
+        close(pip[1]);
+        exit(0);
+    }else{
+        char chaine_papa[]="Je vais etre effacee";
+        int taille_chaine = strlen(chaine_papa);
+        printf("taille %d \n",taille_chaine);
+        memset(chaine_papa,'\0',taille_chaine);
+        printf("Chaine avant %s \n",chaine_papa);
+        close(pip[1]);
+        read(pip[0],chaine_papa,taille_chaine);
+        close(pip[0]);
+        printf("Chaine après %s \n",chaine_papa);
+    }
+}
